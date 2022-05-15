a5c7949b9e0d172386488f1ed07600f2c745ea05
HalfLife <pigoig@gmail.com>
2022-05-15 19:17:50 +0800

    update

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,10 @@
 FROM alpine:edge
 
 RUN apk update && \
-    apk add --no-cache ca-certificates caddy wget jq && \
+    apk add --no-cache ca-certificates caddy wget jq tzdata && \
+    cp -f /usr/share/zoneinfo/Asia/Shanghai /etc/localtime && \
+    echo "Asia/Shanghai" > /etc/timezone && \
+    apk del tzdata && \
     rm -rf /var/cache/apk/*
 
 ADD start.sh /start.sh
--- start.sh
+++ start.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 #config
+num=0
+for user in `echo $AUUID |sed 's#;# #g'`; do
+  num=$(( num+1 ))
+  id="$(echo $user |cut -d, -f1)"
+  if [ $num -eq 1 ]; then
+    AUUID="$id"
+  fi
+  if echo $user |grep -q ','; then
+    email="$(echo $user |cut -d, -f2)"
+  else
+    email="love@rayrayray.com"
+  fi
+  users="$users,\n\ \ \ \ \ \ \ \ \ \ {\"id\": \"$id\",\"email\": \"$email\"}"
+done
+users="$(echo $users |sed 's#^,\\n##')"
 DIR_CONFIG="/usr/share/caddy/$AUUID"
 if [ -z "${PORT}" ];then
   PORT=8080
@@ -17,11 +32,11 @@ wget -O /smartdns $PATHSMARTDNS
 chmod 755 /smartdns
 wget -qO- $CONFIGSMARTDNS | sed "s/\$AUUID/$AUUID/g" >${DIR_CONFIG}/smartdns.conf
 if ping6 2001:4860:4860::8888 -c 4 |grep -q ' ms';then
-  sed '2,$s/^#//' ${DIR_CONFIG}/smartdns.conf
+  sed -i '2,$s/^#//' ${DIR_CONFIG}/smartdns.conf
   IPV6_ON=1
 fi
 sed '/^nameserver/s#nameserver\ *#server #' /etc/resolv.conf|grep '^server ' >>${DIR_CONFIG}/smartdns.conf
-/smartdns -c ${DIR_CONFIG}/smartdns.conf &
+/smartdns -c ${DIR_CONFIG}/smartdns.conf -p /var/run/smartdns.pid &
 #argo
 if [ "${ArgoCERT}" = "ARGOCERT" ]; then
   echo skip
@@ -45,8 +60,16 @@ fi
 #rayrayray
 wget -qO /rayrayray $PATHRAY
 chmod 755 /rayrayray
-wget -qO- $CONFIGRAY | sed "s/\$AUUID/$AUUID/g" >${DIR_CONFIG}/rayrayray.json
+wget -qO- $CONFIGRAY | sed -e "/id.*AUUID/c $users" -e "s/\$AUUID/$AUUID/g" >${DIR_CONFIG}/rayrayray.json
+if [ -n "${GEOSITE}" ] && ! echo ${GEOSITE} |grep -q '#' ;then
+  wget -qO /geosite.dat $GEOSITE
+  sed -i '/geosite.dat/s/#//' ${DIR_CONFIG}/rayrayray.json
+fi
+if [ -n "${GEOIP_CN}" ] && ! echo ${GEOIP_CN} |grep -q '#' ;then
+  wget -qO /cn.dat $GEOIP_CN
+  sed -i '/cn.dat/s/#//' ${DIR_CONFIG}/rayrayray.json
+fi
 if [ -n "$IPV6_ON" ]; then
-  sed -i '/queryStrategy.*UseIPv4/s#\"#//\"#' ${DIR_CONFIG}/rayrayray.json
+  sed -i 's#UseIPv4#UseIP#' ${DIR_CONFIG}/rayrayray.json
 fi
 /rayrayray -config ${DIR_CONFIG}/rayrayray.json
